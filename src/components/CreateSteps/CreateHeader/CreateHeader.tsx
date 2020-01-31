3e6d0d219e8daea4abc96226e7ad86092c6deb08
ildarmgt <ildarmgt@gmail.com>
2020-01-31 17:46:28 -0500

    modified header to use wrapping div to hide overflow for some browsers

--- src/components/CreateSteps/CreateHeader/CreateHeader.tsx
+++ src/components/CreateSteps/CreateHeader/CreateHeader.tsx
@@ -8,6 +8,9 @@ import { Store } from './../../../store/'
 export const CreateHeader = () => {
   const { state } = React.useContext(Store) // global state
 
+  // Make sure entire alias is always visible on top through
+  // resizing based on letter count.
+  //
   // get window width in component instance state 'width'
   // so can resize alias based on char count
   const [ width, setWidth ] = useState(window.innerWidth)
@@ -17,28 +20,38 @@ export const CreateHeader = () => {
     return () => window.removeEventListener('resize', resize);
   }, [])
   const domainName = state.alias + state.extension
+  // enforces max size to font size so 1 letter alias has same font as 19 letter alias
   const fontScale = Math.min(
       Math.floor(1.3 * width / domainName.length),
       Math.floor(1.3 * width / 20)
   )
 
+  const barHeight = { height: (0.12 * width + 0.4 * fontScale).toString() + 'px' }
+  const aliasMarginTop = { marginTop: (0.03 * width - 0.3 * fontScale).toString() + 'px' }
+  const aliasFontSize = { fontSize: fontScale.toString() + 'px' }
+
   return (
     <>
       <div
-        className={ styles.bar }
-        style={{ top: (-0.10 * width - 0.4 * fontScale).toString() + 'px' }}
-      />
+        className={ styles.cutOverflow }
+        style={ barHeight }
+      >
+        <div
+          className={ styles.bar }
+          style={ barHeight }
+        />
+      </div>
       <div
         className={ styles.wrapper }
-        style={{ marginTop: (0.03 * width - 0.3 * fontScale).toString() + 'px' }}
+        style={ aliasMarginTop }
       >
         <span
           className={ styles.alias }
-          style={{ fontSize: fontScale.toString() + 'px' }}
+          style={ aliasFontSize }
         >{ state.alias }</span>
         <span
           className={ styles.ext }
-          style={{ fontSize: fontScale.toString() + 'px' }}
+          style={ aliasFontSize }
         >{ state.extension }</span>
       </div>
       <div className={ styles.spacer } />
