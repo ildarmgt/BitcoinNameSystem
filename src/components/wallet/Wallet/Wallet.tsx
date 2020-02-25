57c79e89d157f20472b9d2a95d04297e7eeb0f9d
ildarmgt <ildarmgt@gmail.com>
2020-02-25 13:29:18 -0500

    wallet comp now accurately logs all params from url

--- src/components/wallet/Wallet/Wallet.tsx
+++ src/components/wallet/Wallet/Wallet.tsx
@@ -9,52 +9,102 @@ import styles from './Wallet.module.css'
  *
  */
 export const Wallet = (props: any): JSX.Element => {
-  // tasks
-  const [tasks, setTasks]: [any, (args: any) => void] = React.useState()
+  // stores new URL
+  // const [newURL, setNewURL]: [any, (args: any) => void] = React.useState()
 
-  // detect changes in url
+  const [params, setParams]: [any, (args: any) => void] = React.useState({})
+
+  // run methods to handle detection and processing of parameters after # in URL
   React.useEffect(() => {
-    const onUrlChange = (e: any) => {
-      console.log(
-        'new url detected:', e.newURL,
-        '\nold:', e.oldURL,
-        '\nevent:', e
-      )
-      setTasks(e.newURL)
+    // url change handler
+    const handleHashChange = () => {
+
+      // convert url string starting with # into key/value pair object
+      // #/ok/?a=b&c=d becomes { a:b, c:d }
+      const fedValues = window.location.hash
+        .split('#')
+        .slice(1) // removes all before 1st # and 1st #
+        .join('')
+        .split('?')
+        .slice(1) // removes all before 1st ? and 1st ?
+        .join('')
+        .split('&') // split between sets of key-value pairs
+        .reduce((finalParamObject: any, thisKeyValue: string) => {
+          // assume values were passed through encodeURIComponent() so only '=' are from standard format
+          const splitKeyValue = thisKeyValue.split('=')
+          const thisKey = splitKeyValue[0]
+          const thisValue = decodeURIComponent(splitKeyValue[1])
+          if (thisKey === '') {
+            // no change
+            return finalParamObject
+          } else {
+            // check if key/value already exist within params
+            if ((thisKey in params) && (params[thisKey] === thisValue)) {
+              // if so, no need to add
+              return finalParamObject
+            } else {
+              // add changes
+              return { ...finalParamObject, [thisKey]: thisValue }
+            }
+          }
+        }, {})
+
+      // only update state if there are new values, avoid pointless rerenders
+      if (Object.keys(fedValues).length > 0) {
+        const newParams = { ...params, ...fedValues }
+        console.log('new wallet params added:', newParams)
+        setParams(newParams)
+      }
     }
+
     // event to detect url change
-    window.addEventListener('hashchange', onUrlChange)
-    // clean up function if removed (or changed)
+    window.addEventListener('hashchange', handleHashChange)
     return () => {
-      window.removeEventListener('hashchange',onUrlChange)
+      // clean up function if removed (or changed)
+      window.removeEventListener('hashchange',handleHashChange)
+      // clean up url as well
+      resetUrl()
     }
-  }, [])
-
-
-  const TESTING = (process.env.NODE_ENV === 'development')
+  }, [params])
 
   return (
-    <div className={ styles.wrapper }>
+    <div className={ styles.wrapper }
+      onClick={ () => {
+        resetUrl()
+      } }
+    >
       { (TESTING) && (
         <>
           w
           { ' ' }
-          { tasks }
         </>
       ) }
     </div>
   )
 }
 
+// remove params from URL
+const resetUrl = () => {
+  window.history.pushState({}, '', `${ window.location.href.split('?')[0] }`)
+  // emit event if param or url change needs to be detected
+  // window.dispatchEvent(new HashChangeEvent("hashchange"));
+}
+
+// if development mode
+const TESTING = (process.env.NODE_ENV === 'development')
+
+
 /**
  *
  * what's best for passing data?
  * - querry strings
- *    - users can accidentally edit or on purpose
- *    - works cross origin
+ *    - users can accidentally edit or on purpose (I like that it's visible and customizable)
+ *    - works cross origin (just put in confirmation checks)
  *    - needs unique path or could be confusing to other components
  *    - confusing url for user (needs cleanup after read imo)
  *    - much less data can be passed (so have to get your own data)
+ *    - if pass ? after #, not querry string so no forced rerender
+ *      and separate from querry string search links
  *
  * - local storage
  *    - cross origin safe
@@ -67,4 +117,22 @@ export const Wallet = (props: any): JSX.Element => {
  *    - same as local storage but erased between browser sessions*
  *
  */
+
+
+// url parameters after #:
+
+// value could have base58 or similar so it's encoded
+// keys are kept simple
+// encodeURIComponent() escapes all characters except: A-Z a-z 0-9 - _ . ! ~ * ' ( )
+// e.g. const randomDataFed1 = 'who1=' + encodeURIComponent('878b8=ABCD-=+_`' + Math.random().toFixed(2))
+
+// window.origin is url up to but not including '/' before the '#'
+// search params are found after '?' but before the '#'
+// window.location.pathname is path after window.origin but before #
+// then the rest is window.hash that includes # and everything after
+// '?' after # are not search params so can be separate
+
+
+// other way to update url:
+// window.history.replaceState({}, '', `${location.pathname}?${params}`);
 
