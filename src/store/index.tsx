5e184922fde916679441eedeca94fcf0c1f492ab
ildarmgt <ildarmgt@gmail.com>
2020-02-24 23:12:43 -0500

    moved initial global state into separate more descriptive file

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,46 +1,12 @@
 import React from 'react'
 import { I_State, BnsBidType } from '../interfaces'
 import reducer from './reducers/Reducer'
-import { newState } from './../helpers/bns/initialState'
+import { initialState } from './initialState'
 
 // Change state process:
 // 1. Component calls action from src/store/actions/
 // 2. Action does stuff (e.g. API calls) and dispatches results to reducer in src/store/reducer/
 // 3. Reducer modifies state based on dispatched payload and type of action.
-
-/* -------------------------------------------------------------------------- */
-/*                                initial state                               */
-/* -------------------------------------------------------------------------- */
-export const initialState: I_State = {
-  network: 'testnet',             // 'testnet' or 'bitcoin'
-  alias: 'satoshi',               // first half of domain name
-  extension: '.btc',              // last half of domain name
-  domain: newState.domain,        // using BNS equivalent domain object
-  wallet: {                       // wallet information & utxo for controlling domain names
-    address: '',                  // public address (p2wpkh)
-    mnemonic: '',                 // mnemonic for private key derivation
-    WIF: '',                      // wallet import format for private key derivation
-    txHistory: [],                // array of all tx for this address
-    utxoList: [],                 // array of all current utxo for this address
-  },
-  chain: {
-    height: 0                     // height of blockchain
-  },
-  pageInfo: {                     // user navigation information for controlling domain name
-    current: 1,
-    checkedDomain: false,         // current domain notification address was scanned
-    checkedWallet: false,         // current wallet address was scanned
-    checkedLightSearch: false     // if light search was done (no utxo / txhex scan)
-  },
-  choices: {                      // choices made by user via this app
-    action: {},                   // choices and data for action to take
-    feeRate: 1.1,                 // fee rate to use in sat/bByte,
-    txHex: '',                    // hex of raw transaction, ideally ready for broadcast
-    embedString: ''               // string to embed in op_return
-  },
-  lastTimeStamp: Date.now(),      // last change timestamp, to detect any changes to state or time out
-}
-
 
 /* -------------------------------------------------------------------------- */
 /*                             Global state setup                             */
