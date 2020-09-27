22fd9992aabd0d4db3bcac6719e1944a7afde2af
ILDAR MUSIN <ildarmgt@gmail.com>
2020-09-27 05:30:12 -0400

    constants refactored slightly to console log values used

--- src/helpers/bns/constants.tsx
+++ src/helpers/bns/constants.tsx
@@ -5,10 +5,11 @@
 // testing mode true/false to reduce waiting time for tests
 // on for `npm run start` (development mode)
 // off for `npm run build` (production mode)
-const TESTING = process.env.NODE_ENV === 'development'
+export const TESTING = process.env.NODE_ENV === 'development'
 TESTING ? console.warn('Testing mode ON') : console.warn('Deployment mode')
 
 // Block times can vary from 10 minutes but ok for estimate.
+// Extremely different results timewise on testnet.
 // blocks * 10 / 60 / 24 ~ days
 // 52560 blocks ~365 days.
 // 15000 blocks ~104 days.
@@ -18,32 +19,32 @@ TESTING ? console.warn('Testing mode ON') : console.warn('Deployment mode')
 
 // max data safe to embed in OP_RETURN type tx (bytes)
 // ~80 chars
-const BYTES_MAX = 80
+export const BYTES_MAX = 80
 
 // Satoshis to burn minimum for attempting ownership (satoshi)
-const MIN_BURN = 1000
+export const MIN_BURN = 1000
 
 //  Minimum to use for notification output (satoshi)
-const MIN_NOTIFY = 1000
+export const MIN_NOTIFY = 1000
 
 // Ownership duration after victory (blocks)
 // ~1 year (production) / ~104 days (development)
-const OWNERSHIP_DURATION_BY_BLOCKS = !TESTING ? 52560 : 15000
+export const OWNERSHIP_DURATION_BY_BLOCKS = !TESTING ? 52560 : 15000
 
 // Duration before a top bidder becomes owner (blocks)
 // ~1 day
-const CHALLENGE_PERIOD_DURATION_BY_BLOCKS = 144
+export const CHALLENGE_PERIOD_DURATION_BY_BLOCKS = 144
 
 // Multiplier required on burn amount for new bid to beat
 // a previous (lower height) winning bid (assuming meets other conditions by deadline).
 // sets a minimum treshhold for trying to outbid someone else
-const CHALLENGE_MIN_MULTIPLY = 2
+export const CHALLENGE_MIN_MULTIPLY = 2
 
 // BNS did not really exist before this point (for notifications only, wallets can go further)
 // this might be used to feed to nodes to speed up fetching of notification data to only after this height
-const EARLIEST_NOTIFICATION_HEIGHT = 1662000
+export const EARLIEST_NOTIFICATION_HEIGHT = 1662000
 
-export {
+console.log('CONSTANTS:', {
   TESTING,
   MIN_BURN,
   MIN_NOTIFY,
@@ -52,4 +53,4 @@ export {
   CHALLENGE_PERIOD_DURATION_BY_BLOCKS,
   CHALLENGE_MIN_MULTIPLY,
   EARLIEST_NOTIFICATION_HEIGHT
-}
+})
