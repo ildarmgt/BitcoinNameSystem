b910a4c7ff87e58cbf2031565bda48445aed926c
ILDAR MUSIN <ildarmgt@gmail.com>
2020-09-27 05:57:46 -0400

    cleaned up settings page

--- src/components/SettingsContent/SettingsContent.tsx
+++ src/components/SettingsContent/SettingsContent.tsx
@@ -13,9 +13,8 @@ export const SettingsContent = () => {
     <div className={[styles.wrapper, 'scrollbar'].join(' ')}>
       <div className={styles.contentWrapper}>
         <div className={styles.title}>Settings</div>
-        <div>(todo)</div>
-        subject to changes until launch high chance of bugs and losses,
-        especially until reviewed
+        Subject to changes until beta. <br />
+        Not yet reviewed: high chance of bugs and losses. <br />
         <div>
           Network:
           <Switch
@@ -59,8 +58,7 @@ export const SettingsContent = () => {
           {state.network} : {state.api.path[state.network]}
         </InputForm>
         <br />
-        <br />
-        <div>
+        {/* <div>
           Automatically save data to browser (local storage) ? (todo)
           <br />
           <Switch choices={[{ value: 'not allow' }, { value: 'allow' }]} />
@@ -69,7 +67,7 @@ export const SettingsContent = () => {
           (if yes) Provide a password to encrypt local storage <br />
           with to hide it from other applications (todo)
           <br /> [todo textbox form]
-        </div>
+        </div> */}
       </div>
     </div>
   )
